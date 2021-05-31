f8a4ea18d228fbf1634369fe66639d1f2e3bcc93
Electogenius <69585636+Electogenius@users.noreply.github.com>
2021-05-31 20:11:37 +0530

    1337 haxxor

---
+++ projects/1337__H4XX0R__/h4xx_.js
@@ -0,0 +1,51 @@
+// 1337__H4XX0R__, a language only for the 1337__H4XX0RZ
+
+try {
+	this.$__próćéśś__ = {
+		"_1337__h4><_70015": {
+			"initiate::modular_hypoquadsubtantiater;;framework": (e, c) => {
+				if (e != "irregular superdisplacementionate") $__próćéśś__.throww()
+				let j = c.split(/\n/)
+				if (j[0] != "create _v01d public instance namespace()(__argv5)[]{") $__próćéśś__.throww()
+				if (j[1] != $__próćéśś__.dayinstantiateclass()) $__próćéśś__.throww()
+				if (j[3] != "}end process [[main(thread)(|- " + $__próćéśś__.location() + " -|)]]") $__próćéśś__.throww()
+				if (j[2] != "										process.module.system.input|output##:printLinearRTLTTB::{locale===process.world.countries._us.en}((’process.module.Function__gr337‘))") {
+					switch (j[2]) {
+						default:
+							$__próćéśś__.throww()
+							break;
+					}
+				} else {
+					console.warn("1337__H4XX0R__ must be used for more advanced uses than a silly 'hello world'. Nevertheless running program.")
+					console.log("__H3110.!w0R1)")
+				}
+				return c
+			}
+		},
+		throww: () => {
+			throw $__próćéśś__.err[Math.floor(Math.random() * $__próćéśś__.err.length)]
+		},
+		err: [
+		"__typeanalysis macromodulator says: unterminated nanohyperbolator.",
+		"!!:proto interpolator does not match collaterally detached modules.",
+		"unexpected analytic error: module exceeded over stack maximum size conservation limit (ISO secret.1337 act, 50:76).",
+		"Parser expected terminal-powered data type consistency but received shelled type inconsistency.",
+		"Received 403 error due to cross-origin re-assesment datatrack protection in intended client server.",
+		"Error while producing typeerror.",
+		"Could not access server IP due to firewall limitations."
+	],
+		dayinstantiateclass: () => "$^**@$[[@instantiate;;;class--set|- " + $__próćéśś__.days[new Date().getDay()] + " -|]]",
+		days: ["5014r", "1Un4r", "1312n1(", "(U))3)", "7hU1)", "n0n1111()", "5014r"],
+		location: function() {
+			try {
+				return window.location
+			} catch (e) {
+				try {
+					return process.mainModule.path
+				} catch (e) { return "unterminated_system_path_" }
+			}
+		}
+	}
+} catch (e) {
+	throw "system unsupported."
+}
